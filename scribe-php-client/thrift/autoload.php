9c8b0adfdb1f1e03a77a18ee8b4dd9a44fde1e04
root <root@blazej.(none)>
2011-07-07 17:04:17 +0200

    Added scribe files

---
+++ scribe-php-client/thrift/autoload.php
@@ -0,0 +1,11 @@
+<?php
+function __autoload($class_name)   {
+    $directory_array = Array('protocol', 'transport');
+    foreach ($directory_array as $dir)  {
+        $file = $GLOBALS['THRIFT_ROOT'] . '/' . $dir . '/' . $class_name . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+        var_dump($file);
+    }
+}
